05e47b0b0637c09f8e9cb9eccd2c7a510531c1a5
omarcharif <omarcharif83@gmail.com>
2026-02-12 20:03:23 -0700

    added SVC import

--- ml_framework_project/models/classifier.py
+++ ml_framework_project/models/classifier.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 class Classifier(ABC):
     def __init__(
